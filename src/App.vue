b840ddd6369148d50fdc76a1c3dddacc9d026eed
Nitin <nd185089@ncr.com>
2021-03-29 10:54:15 -0500

    Checked in the initial changes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MainBoard msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import MainBoard from './components/mainboard.vue';
+import { gethostdetails } from './machineinfo';
 
 @Component({
   components: {
-    HelloWorld,
+    MainBoard,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public class MachineDetail {
+     OSName: string;
+     TotalMemory: string;
+     FreeMemory: string;
+  }
+  public OSName: string = "";
+  public TotalMemory: string = "";
+  public FreeMemory: string = "";
+  public created() {
+    this.MachineDetail = new MachineDetail();
+    gethostdetails()
+      .then((res) => {
+        console.log(res);
+        this.OSName = res["OS Platform"];
+        this.TotalMemory = res["Total Memory"];
+        this.FreeMemory = res["Free Memory"];
+      });
+  }
+}
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import * as os from 'os';
+import platform from 'platform';
 
 Vue.config.productionTip = false;
+
+import './../node_modules/bulma/css/bulma.css';
+
+
 
 new Vue({
   store,
