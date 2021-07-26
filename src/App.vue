af4d71a24b1d509816e8b0fb962dbf96e70a5719
Nitin <nd185089@ncr.com>
2021-07-26 12:26:18 -0500

    Implemented unit testing using jest and vue utils test

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,71 @@
 <template>
   <div id="app">
-    <MainBoard msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MainBoard :finalmachine="MainMachine" />
   </div>
 </template>
+
+
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import MainBoard from './components/mainboard.vue';
 import { gethostdetails } from './machineinfo';
 
+interface MachineType  {
+    OSName: string;
+    TotalMemory: string;
+    FreeMemory: string;
+    HostName: string;
+    Platform: string;
+    Architecture: string;
+    CPU: string[];
+    IPAddress: any[];
+}
+
 @Component({
   components: {
     MainBoard,
   },
 })
+
 export default class App extends Vue {
-  public class MachineDetail {
-     OSName: string;
-     TotalMemory: string;
-     FreeMemory: string;
-  }
-  public OSName: string = "";
-  public TotalMemory: string = "";
-  public FreeMemory: string = "";
+  public MainMachine: MachineType = {
+    OSName: "",
+    TotalMemory: "",
+    FreeMemory: "",
+    HostName: "",
+    Platform: "",
+    Architecture: "",
+    CPU: [""],
+    IPAddress: [{}],
+  };
   public created() {
-    this.MachineDetail = new MachineDetail();
     gethostdetails()
       .then((res) => {
-        console.log(res);
-        this.OSName = res["OS Platform"];
-        this.TotalMemory = res["Total Memory"];
-        this.FreeMemory = res["Free Memory"];
+              this.MainMachine.OSName = res["OS Platform"];
+              this.MainMachine.TotalMemory = res["Total Memory"];
+              this.MainMachine.FreeMemory = res["Free Memory"];
+              this.MainMachine.HostName = res[`Host`];
+              this.MainMachine.Platform = res["OS Platform"];
+              this.MainMachine.Architecture = res["OS Archtecture"];
+              const cpujson = res[`CPUS`];
+              for (const cpurecord in cpujson) {
+                  if (cpujson.hasOwnProperty(cpurecord)) {
+                      const proprecord = cpujson[cpurecord];
+                      this.MainMachine.CPU.push(proprecord[`model`]);
+                  }
+
+              }
+              const ipinfo = res[`IPAddress`];
+              const wifi = JSON.parse(JSON.stringify(ipinfo["Wi-Fi"]));
+              this.MainMachine.IPAddress.length = 0;
+              for (const iprecord in wifi) {
+                  if (!iprecord.hasOwnProperty(wifi)) {
+                      const record = wifi[iprecord];
+                      this.MainMachine.IPAddress.push(record);
+                  }
+              }
+        }
       });
   }
 }
